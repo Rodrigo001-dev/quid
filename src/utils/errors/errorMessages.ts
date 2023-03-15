ec557ebdabf78d56165eba65124b33399c982ae5
Rodrigo <rodrigorael53@gmail.com>
2023-03-15 03:27:02 -0300

    :goal_net: feat: add type errors

---
+++ src/utils/errors/errorMessages.ts
@@ -0,0 +1,7 @@
+export const errorMessages = {
+  methodNotAllowed: "Method not allowed",
+  invalidBody: "Invalid request body",
+  pendingPayment: "customer already has a pending payment",
+  errorClientIdOrEupagoAPI:
+    "invalid client id or eupago API did not return success status code",
+};
